bd681cac2e057f1ba47a0ce7d8bbbd77e5680d5b
Dayn Augustson <Daugustson@cnm.edu>
2018-10-18 12:20:38 -0600

    start of foo.php class work.

---
+++ php/Classes/Foo.php
@@ -0,0 +1,17 @@
+<?php
+namespace daugustson\DataDesign;
+
+
+
+class shopping_cart {
+      /**
+		 * *this ID for the shopCartProfile:
+		 * this is a foreign key that is references to
+		 * profile(profileID);
+		 * @var Uuid shopCartProfileID
+		 */
+      private shopCartProfileID;
+
+
+}
+
