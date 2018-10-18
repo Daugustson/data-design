e78fe19709bce155d5acfea9ac27a1bff080ba53
Dayn Augustson <Daugustson@cnm.edu>
2018-10-18 13:07:32 -0600

    set the class

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -7,10 +7,35 @@ class shopping_cart {
       /**
 		 * *this ID for the shopCartProfile:
 		 * this is a foreign key that is references to
-		 * profile(profileID);
+		 * profile(profileID)
 		 * @var Uuid shopCartProfileID
 		 */
-      private shopCartProfileID;
+      private $shopCartProfileID;
+      /**
+		 * this ID is for the shopCartProductMrfNum:
+		 *this is a foreign key that is references to
+		 *product(productMrfNumID)
+		 * @var Uuid shopCartProductMrfNumID
+		 */
+      private $shopCartProductMrfNumID;
+      /**
+		 * this is the quantity of product in the shopping cart
+		 * @var string
+		 */
+      private $shopCartQuantity;
+	/**
+	 * this is the part number for the product in the shopping cart
+	 * @var string
+	 */
+	    private $shopCartPartNumber;
+	/**
+	 *this is the customer reference for this order.
+	 * @var string
+	 */
+       private $shopCartCustomerReference;
+
+
+
 
 
 }
